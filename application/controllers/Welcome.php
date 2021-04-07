0994f4b13dd1520e95a9db1fc9d1ee38dcb954be
KRAB <developer@kabhishek18.com>
2021-04-07 16:18:17 +0530

    Mongo db Integration

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -7,6 +7,7 @@ class Welcome extends CI_Controller {
 		$this->load->library('cart');
 		$this->load->helper('file');
 		$this->load->model('test_model');
+		$this->load->library('mongo_db', array('activate'=>'newdb'),'mongo_db2');
 		$this->load->library('session');
 		$this->load->helper('date');
 		if ($this->config->item('secure_site')) {
@@ -14,126 +15,63 @@ class Welcome extends CI_Controller {
 		}
 	}
 
-	public function index()
+	public function ApiPullStore()
 	{
-		$curl_handle = curl_init();
-		curl_setopt_array($curl_handle, array(
-		  CURLOPT_URL => "https://apilisting.fragrancex.com/product/list/",
-		  CURLOPT_RETURNTRANSFER => true,
-		  CURLOPT_SSL_VERIFYHOST =>false,
-		  CURLOPT_SSL_VERIFYPEER => false,
-		  CURLOPT_ENCODING => "",
-		  CURLOPT_MAXREDIRS => 10,
-		  CURLOPT_TIMEOUT => 30,
-		  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-		  CURLOPT_CUSTOMREQUEST => "GET",
-		  CURLOPT_POSTFIELDS => "grant_type=apiAccessKey&apiAccessId=6dd281e78f26&apiAccessKey=7f314f940fa8835495eb9c5c1f5d5be51875e564",
-		  CURLOPT_HTTPHEADER => array(
-		    "content-type: application/json","Authorization: Bearer C0cf_08z7uul-h7MDhhqMTxnQuDkdMiqlq4rHbV3CWeoPd7tG1tAnuXigvn_rYE2dzcAWQ7SUcQjkshbWngtgU9qhYi1Id8nFav-y9KlAwq-LDKUlUutZDox3SPFTKs031Smyq2OH1VVxebLEvRx4mvQHuFUnOldRh4t5UlvNpYCsDxh-1RMRGW2LqFmUrweM-AE4_NbYBQSor4im2SE4RfnEFLQ5bZLisPJC0cAFAq6d_QAh15Jh5ggioAcMLEY3a5nlDuRzF0BcVp0cmVRoPFLz_VTCdNrfQb_i91U9LxHs6XY",
-		  ),
-		));
-		// Execute curl & store data in a variable
-		$curl_data = curl_exec($curl_handle);
-
-		curl_close($curl_handle);
-
-		// Decode JSON into PHP array
-		$response_data = json_decode($curl_data,true);
-
-		var_dump(count($response_data));
-		// Print all data if needed
-		$dir =FCPATH.'productlog/';
+		$response = Fragnance_getfile(Fragnancex_accesstoken());
+		$dir =FCPATH.'productlog';
 		if (!is_dir($dir)) {
 			mkdir($dir, 0777, TRUE);
 		}
-		if (write_file( FCPATH.'productlog/main.json', $curl_data) == FALSE)
+		if (write_file( $dir.'/main.json', $response) == FALSE)
 		{
 		   echo 'Unable to write the file';
 
 		} else {
-			write_file( FCPATH.'productlog/'.date('Y-m-d H:i:s').'.json', $curl_data) ;
-		    echo 'File written!';                           
+			write_file( FCPATH.'productlog/'.date('Y-m-d H:i:s').'.json', $response);                     
 		}
 	}
 
-	public function Access_token($value='')
-	{
-		$curl_handle = curl_init();
-
-		curl_setopt_array($curl_handle, array(
-		  CURLOPT_URL => "https://apilisting.fragrancex.com/token",
-		  CURLOPT_RETURNTRANSFER => true,
-		  CURLOPT_SSL_VERIFYHOST =>false,
-		  CURLOPT_SSL_VERIFYPEER => false,
-		  CURLOPT_ENCODING => "",
-		  CURLOPT_MAXREDIRS => 10,
-		  CURLOPT_TIMEOUT => 30,
-		  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-		  CURLOPT_CUSTOMREQUEST => "POST",
-		  CURLOPT_POSTFIELDS => "grant_type=apiAccessKey&apiAccessId=6dd281e78f26&apiAccessKey=7f314f940fa8835495eb9c5c1f5d5be51875e564",
-		  CURLOPT_HTTPHEADER => array(
-		    "content-type: application/json"
-		  ),
-		));
-		// Execute curl & store data in a variable
-		$curl_data = curl_exec($curl_handle);
-
-		curl_close($curl_handle);
-
-		// Decode JSON into PHP array
-		$response_data = json_decode($curl_data);
-
-		// Print all data if needed
-		echo "<pre>";
-		 print_r($response_data);
-	}
-	public function Test($value='')
+	
+	public function StoreDB()
 	{	
 		$data = read_file(FCPATH.'productlog/main.json');
 		$data = json_decode($data,true);
-			$i =1;
-		$value="";	
-		foreach ($data as $key) {
-           $insert =$this->test_model->InsertOrder($key);
-            if($insert){
-            	echo $i++;
-            	echo "<br>";
-            }else{
-            	echo "false";
-            }
+		$i=1;
+		$datareset = $this->mongo_db2->deleteAll('products');
+		if($datareset){
+			foreach ($data as $key) {
+				$insert =$this->mongo_db2->insert('products',$key);
+				if($insert){
+					echo 'Product '.$i++.' inserted';
+					echo "<br>";
+				}else{
+					echo "false";
+				}
+			}
+		}else{
+			echo "Unable To Reset The Collections";
 		}
-
 	}
 
 
-	public function brand()
+	public function GetAllProducts()
 	{
-		$curl_handle = curl_init();
-		curl_setopt_array($curl_handle, array(
-		  CURLOPT_URL => "https://apilisting.fragrancex.com/product/list/60305M",
-		  CURLOPT_RETURNTRANSFER => true,
-		  CURLOPT_SSL_VERIFYHOST =>false,
-		  CURLOPT_SSL_VERIFYPEER => false,
-		  CURLOPT_ENCODING => "",
-		  CURLOPT_MAXREDIRS => 10,
-		  CURLOPT_TIMEOUT => 30,
-		  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-		  CURLOPT_CUSTOMREQUEST => "GET",
-		  CURLOPT_POSTFIELDS => "grant_type=apiAccessKey&apiAccessId=6dd281e78f26&apiAccessKey=7f314f940fa8835495eb9c5c1f5d5be51875e564",
-		  CURLOPT_HTTPHEADER => array(
-		    "content-type: application/json","Authorization: Bearer C0cf_08z7uul-h7MDhhqMTxnQuDkdMiqlq4rHbV3CWeoPd7tG1tAnuXigvn_rYE2dzcAWQ7SUcQjkshbWngtgU9qhYi1Id8nFav-y9KlAwq-LDKUlUutZDox3SPFTKs031Smyq2OH1VVxebLEvRx4mvQHuFUnOldRh4t5UlvNpYCsDxh-1RMRGW2LqFmUrweM-AE4_NbYBQSor4im2SE4RfnEFLQ5bZLisPJC0cAFAq6d_QAh15Jh5ggioAcMLEY3a5nlDuRzF0BcVp0cmVRoPFLz_VTCdNrfQb_i91U9LxHs6XY",
-		  ),
-		));
-		// Execute curl & store data in a variable
-		$curl_data = curl_exec($curl_handle);
-
-		curl_close($curl_handle);
-
-		// Decode JSON into PHP array
-		$response_data = json_decode($curl_data,true);
-
-		var_dump($response_data);
-		// Print all data if needed
-	
+		$res =$this->mongo_db2->get('products');
+		echo "<pre>";
+		print_r($res);
+	}
+	public function lolinsert()
+	{
+		$res =$this->mongo_db2->insert('products',$array);
+		echo "<pre>";
+		print_r($res);
+	}
+	public function lolUpdate()
+	{
+		$this->mongo_db2->where('username','asd');
+		$this->mongo_db2->get('users');
+		$res =$this->mongo_db2->get('users');
+		echo "<pre>";
+		print_r($res);
 	}
 }
--- application/helpers/kabhishek_helper.php
+++ application/helpers/kabhishek_helper.php
@@ -5,40 +5,41 @@
 #generateUUID
 #MessageAlertStatus
 #DD(Direct Dump)
+#Create Access Token Fragnancex_accesstoken
+#Fragnance_getfile
 */
-if ( ! function_exists('force_ssl'))
-{
-	function force_ssl() {		
-		if (!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] != "on") {
-			$url = "https://". $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
-			redirect($url);
-			exit;		
-		}	
+	if ( ! function_exists('force_ssl'))
+	{
+		function force_ssl() {		
+			if (!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] != "on") {
+				$url = "https://". $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+				redirect($url);
+				exit;		
+			}	
+		}
 	}
-}
 
 
 
-if ( ! function_exists('generateUUID'))
-{
-	function generateUUID(){
-		$charid = md5(uniqid(rand(), true).time());
-		$hyphen = chr(45);// "-"
-		$uuid = substr($charid, 0, 8).$hyphen
-		  .substr($charid, 8, 4).$hyphen
-		  .substr($charid,12, 4).$hyphen
-		  .substr($charid,16, 4).$hyphen
-		  .substr($charid,20,12);
-		return $uuid;
+	if ( ! function_exists('generateUUID'))
+	{
+		function generateUUID(){
+			$charid = md5(uniqid(rand(), true).time());
+			$hyphen = chr(45);// "-"
+			$uuid = substr($charid, 0, 8).$hyphen
+			.substr($charid, 8, 4).$hyphen
+			.substr($charid,12, 4).$hyphen
+			.substr($charid,16, 4).$hyphen
+			.substr($charid,20,12);
+			return $uuid;
+		}
 	}
-}
 
 
 
 
-if ( !function_exists('MessageAlertStatus')) {
-		
-
+	if ( !function_exists('MessageAlertStatus')) 
+	{
 		function MessageAlertStatus($success,$status,$message,$extra = []){
 				  return array_merge([
 				        'success' => $success,
@@ -48,103 +49,83 @@ if ( !function_exists('MessageAlertStatus')) {
 		}
 	}
 
-if (!function_exists('dd')) {
- function dd()
-  {
-      echo '<pre>';
-      array_map(function($x) {var_dump($x);}, func_get_args());
-      die;
-   }
- }
 
 
-if(!function_exists('EmailMessage')){
-	function EmailMessage($user,$link){
+	if (!function_exists('dd')) 
+	{
+		function dd()
+		{
+			echo '<pre>';
+			array_map(function($x) {var_dump($x);}, func_get_args());
+			die;
+		}
+ 	}
 
-		$template ='
-					<table width="100%" cellpadding="0" cellspacing="0" border="0" id="m_-2287190302310609224m_-7533971164095270638background-table" style="border-collapse:collapse;padding:0;margin:0 auto;background-color:#ebebeb;font-size:12px">
-					   <tbody>
-					      <tr>
-					         <td valign="top" align="center" style="font-family:calibri;font-weight:normal;border-collapse:collapse;vertical-align:top;padding:0;margin:0;width:100%">
-					            <table cellpadding="0" cellspacing="0" border="0" align="center" style="border-collapse:collapse;padding:0;margin:0 auto;width:600px">
-					               <tbody>
-					                  <tr>
-					                     <td align="center" style="background:#fff;font-family:calibri;font-weight:normal;border-collapse:collapse;vertical-align:top;padding:0;margin:0">
-					                        <table cellpadding="0" cellspacing="0" border="0" style="border-collapse:collapse;padding:0;margin:0">
-					                           <tbody>
-					                              <tr>
-					                                 <td align="center"  style=" font-family:calibri;font-weight:normal;border-collapse:collapse;vertical-align:top;padding:15px 0px 10px 5px;margin:0">
-					                                    <a href="https://go2bng.com/" style="color:#3696c2;float:left;display:block" rel="noreferrer" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://go2bng.com/&amp;source=gmail&amp;ust=1612437442476000&amp;usg=AFQjCNGp8vRHo85GtG1KT4EjwDV7Yqv0Lg">
-					                                    <img width="50%" height="" src="https://go2bng.com/resource/images/logo.png" alt="Go2bng.com" border="0" style=" outline:none;text-decoration:none" class="CToWUd"></a>
-					                                 </td>
-					                              </tr>
-					                           </tbody>
-					                        </table>
-					                     </td>
-					                  </tr>
-					                  <tr>
-					                     <td align="center" style="background:#fff;font-family:calibri;font-weight:normal;border-collapse:collapse;vertical-align:top;padding:0;margin:0">
-					                        <table cellpadding="0" cellspacing="0" border="0" style="border-collapse:collapse;padding:0;margin:0;border-top: 3px solid #ffce10; ">
-					                           <tbody>
-					                           
-					                              <tr>
-					                                 <td style="font-family:calibri;font-weight:normal;border-collapse:collapse;vertical-align:top;padding:5px 15px;margin:0;">
-					                                    <h3 style="text-align:left;margin:0;padding:5px 15px">Dear '.$user.'</h3>
-					                                    <h3 style="padding:5px 15px;font-family:calibri;font-weight:normal;font-size:17px;margin-bottom:10px;margin-top:10px">
-					                                    	You are at final step to create account with please click on button to verify your email.
-					                                    </h3>
-					                                    
-					                                 </td>
-					                              </tr>
-					                              <tr>
-					                                 <td style="width: 650px; font-family:calibri;font-weight:normal;border-collapse:collapse;vertical-align:top;padding:0;margin:0">
-					                                    <table bgcolor="" width="100%" height="100px">
-					                                       <tr>
-					                                          <td></td>
-					                                            <td colspan="3" style="color:#000; font-family:calibri;font-weight:normal;border-collapse:collapse;vertical-align:top;padding:5px 15px;margin:0;text-align:center">
-					                                            	<h3 style="font-family:calibri;font-weight:normal;font-size:32px;margin-bottom:10px;margin-top:10px;text-align: center;">
-					                                            	<a href="'.$link.'" style="background: #f19220;color:white;padding: 15px;">
-						                                             	Click To Verify
-					                                            	</a>
-						                                            </h3>
 
-					                                          </td>
-					                                          <td></td>
+	if(!function_exists('Fragnancex_accesstoken'))
+	{
+		function Fragnancex_accesstoken()
+		{
+			$curl_handle = curl_init();
 
-					                                       </tr>
-					                                 
-					                                    </table>
-					                                 </td>
-					                                
-					                                 </td>
-					                              </tr>
-					                           </tbody>
-					                        </table>
-					                        <table cellpadding="0" cellspacing="0" border="0" style="border-collapse:collapse;padding:0;margin:0;border-bottom: 3px solid #ffce10; "> 
-					                           <tbody>
-					                              <tr>
-					                                  <td style="font-family:calibri;font-weight:normal;border-collapse:collapse;vertical-align:top;padding:10px 15px;margin:10px;border-bottom: 3px solid #ffce10;">
-					                                       <p>And as always, if you have questions or feedback for us, we love hearing from you.<br/>
-					                                       	Cheers,<br/>
-					                                       Bng Team</p>
-					                                   </td>        
-					                              </tr>
-					                           </tbody>
-					                        </table>
-					                     </td>
-					                  </tr>
-					               </tbody>
-					            </table>
-					   
-					         </td>
-					      </tr>
-					   </tbody>
-					</table>';
+			curl_setopt_array($curl_handle, array(
+			CURLOPT_URL => "https://apilisting.fragrancex.com/token",
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_SSL_VERIFYHOST =>false,
+			CURLOPT_SSL_VERIFYPEER => false,
+			CURLOPT_ENCODING => "",
+			CURLOPT_MAXREDIRS => 10,
+			CURLOPT_TIMEOUT => 30,
+			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			CURLOPT_CUSTOMREQUEST => "POST",
+			CURLOPT_POSTFIELDS => "grant_type=apiAccessKey&apiAccessId=6dd281e78f26&apiAccessKey=7f314f940fa8835495eb9c5c1f5d5be51875e564",
+			CURLOPT_HTTPHEADER => array(
+				"content-type: application/json"
+			),
+			));
+			// Execute curl & store data in a variable
+			$curl_data = curl_exec($curl_handle);
 
-		return $template;			
+			curl_close($curl_handle);
+
+			// Decode JSON into PHP array
+			$response_data = json_decode($curl_data);
+
+			// Print all data if needed
+			
+			 return $response_data->access_token;
+		}
 	}
-}
 
 
+	if(!function_exists('Fragnance_getfile')){
+		function Fragnance_getfile($var = '')
+		{
+			$curl_handle = curl_init();
+		curl_setopt_array($curl_handle, array(
+		  CURLOPT_URL => "https://apilisting.fragrancex.com/product/list/",
+		  CURLOPT_RETURNTRANSFER => true,
+		  CURLOPT_SSL_VERIFYHOST =>false,
+		  CURLOPT_SSL_VERIFYPEER => false,
+		  CURLOPT_ENCODING => "",
+		  CURLOPT_MAXREDIRS => 10,
+		  CURLOPT_TIMEOUT => 30,
+		  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		  CURLOPT_CUSTOMREQUEST => "GET",
+		  CURLOPT_POSTFIELDS => "grant_type=apiAccessKey&apiAccessId=6dd281e78f26&apiAccessKey=7f314f940fa8835495eb9c5c1f5d5be51875e564",
+		  CURLOPT_HTTPHEADER => array(
+		    "content-type: application/json","Authorization: Bearer $var",
+		  ),
+		));
+		// Execute curl & store data in a variable
+		$curl_data = curl_exec($curl_handle);
+
+		curl_close($curl_handle);
+
+		// Decode JSON into PHP array
+		return $curl_data;
+		
+		}
+	}
 
 ?>
